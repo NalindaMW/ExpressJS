982cc27bafa1e8b02af0a91fcade5ebc6caea1b7
Nalinda Wickramarathna <mastersilicon@gmail.com>
2024-02-26 00:39:43 +0530

    Route Params

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,36 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 1, userName: "nalz", displayName: "Nalinda" },
+  { id: 2, userName: "john", displayName: "John" },
+  { id: 3, userName: "risa", displayName: "Risa" },
+  { id: 4, userName: "adam", displayName: "Adam" },
+  { id: 5, userName: "tima", displayName: "Tima" },
+  { id: 6, userName: "jason", displayName: "Jason" },
+  { id: 7, userName: "marlan", displayName: "Marlan" },
+];
+
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello!" });
 });
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, userName: "nalz", displayName: "Nalinda" },
-    { id: 2, userName: "john", displayName: "John" },
-    { id: 3, userName: "risa", displayName: "Risa" },
-  ]);
+  response.send(mockUsers);
+});
+
+//Route Params
+//Ex: -> /api/users/1
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+
+  if (isNaN(parsedId))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+
+  const findUser = mockUsers.find((user) => user.id == parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
 });
 
 app.get("/api/products", (request, response) => {
