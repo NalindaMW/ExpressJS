1ac5fdc13496e1aeb8adbb05ecf5081568785d9a
Nalinda Wickramarathna <mastersilicon@gmail.com>
2024-02-26 23:06:29 +0530

    Query params

--- src/index.mjs
+++ src/index.mjs
@@ -5,21 +5,40 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
-  { id: 1, userName: "nalz", displayName: "Nalinda" },
-  { id: 2, userName: "john", displayName: "John" },
-  { id: 3, userName: "risa", displayName: "Risa" },
-  { id: 4, userName: "adam", displayName: "Adam" },
-  { id: 5, userName: "tima", displayName: "Tima" },
-  { id: 6, userName: "jason", displayName: "Jason" },
-  { id: 7, userName: "marlan", displayName: "Marlan" },
+  { id: 1, username: "nalz", displayName: "Nalinda" },
+  { id: 2, username: "john", displayName: "John" },
+  { id: 3, username: "risa", displayName: "Risa" },
+  { id: 4, username: "adam", displayName: "Adam" },
+  { id: 5, username: "tima", displayName: "Tima" },
+  { id: 6, username: "jason", displayName: "Jason" },
+  { id: 7, username: "marlan", displayName: "Marlan" },
 ];
 
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello!" });
 });
 
+//get route without route or query params
+/*
 app.get("/api/users", (request, response) => {
   response.send(mockUsers);
+});
+*/
+
+//Query Params
+//Ex: -> /api/users?filter=username&value=nalz
+app.get("/api/users", (request, response) => {
+  console.log(request.query);
+  const { filter, value } = request.query;
+
+  // When both filter and value are present
+  if (filter && value) {
+    return response.send(
+      mockUsers.filter((user) => user[filter].includes(value))
+    );
+  }
+
+  return response.send(mockUsers);
 });
 
 //Route Params
